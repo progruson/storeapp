c30e7f9bf4e7f910f93a080d52383ff9793f7f94
ProgRus <progruson@gmail.com>
2017-01-15 22:10:41 +0700

    (m) class Item

--- item.rb
+++ item.rb
@@ -1,12 +1,25 @@
 class Item
 
+  def initialize
+    @price = 30 # object property
+  end
+
+  # getter
   def price
-    rand(100)
+    @price
+  end
+
+  #setter
+  def price=(price_value)
+    @price = price_value
   end
 
 end
 
 item1 = Item.new
-item2 = Item.new
 puts item1.price
-puts item2.price
+
+item1.price = 10 # or item1.price=(10)
+puts item1.price
+item1.price = 20
+puts item1.price
