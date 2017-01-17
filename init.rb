76960e057469d348a6587812cddb3630f6bcb608
ProgRus <progruson@gmail.com>
2017-01-17 15:45:45 +0700

    (+) method info

--- init.rb
+++ init.rb
@@ -1,15 +1,5 @@
 require_relative "cart"
 require_relative "item"
 
-cart = Cart.new
-
-cart.add_item(Item.new)
-cart.add_item(Item.new)
-
-p cart.items
-
-cart.remove_item
-
-p cart.items
-
-cart.validate
+item = Item.new({:price => 10, :weight => 100, :name => 'Car'})
+item.info { |attr| puts attr }
--- item.rb
+++ item.rb
@@ -3,9 +3,16 @@ class Item
   def initialize(options={})
     @price  = options[:price]  # object property
     @weight = options[:weight] # object property
+    @name   = options[:name]   # object property
   end
 
-  attr_reader :price, :weight # getters
-  attr_writer :price          # setters
+  attr_reader :price, :weight, :name # getters
+  attr_writer :price                 # setters
+
+  def info
+    yield(price)
+    yield(weight)
+    yield(name)
+  end
 
 end
