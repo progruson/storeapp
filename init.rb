d7f0529d97686f42b3fb97307f75dcebc8081080
ProgRus <progruson@gmail.com>
2017-01-20 19:33:28 +0700

    Read and write files

--- init.rb
+++ init.rb
@@ -6,6 +6,6 @@ require_relative "virtual_item"
 require_relative "real_item"
 
 @items = []
-@items << VirtualItem.new({:price => 101, :weight => 100, :name => 'car'})
+@items << RealItem.new({:price => 101, :weight => 100, :name => 'car'})
 @items << RealItem.new({:weight => 100, :price => 101, :name => 'kettle'})
 @items << RealItem.new({:weight => 100, :price => 101, :name => 'dishwasher'})
