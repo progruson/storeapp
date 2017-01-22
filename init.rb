180ad76a0e81814a8b0d41892f63ea18f745195e
ProgRus <progruson@gmail.com>
2017-01-22 16:25:39 +0700

    (+) singleton StoreApplication

--- init.rb
+++ init.rb
@@ -1,21 +1,19 @@
-require_relative "string"
-require_relative "item_container"
-require_relative "item"
-require_relative "virtual_item"
-require_relative "real_item"
-require_relative "antique_item"
-require_relative "cart"
-require_relative "order"
+require_relative "store_application"
+
+puts StoreApplication.new == StoreApplication.new
+p StoreApplication.new
 
 @items = []
 @items << AntiqueItem.new("car", price: 101, weight: 100)
 @items << RealItem.new({:weight => 100, :price => 101, :name => 'kettle'})
 @items << RealItem.new({:weight => 100, :price => 101, :name => 'dishwasher'})
 
+@items.each { |i| puts i.name }
+
 cart = Cart.new("ruslan")
 cart.add_item RealItem.new({:weight => 100, :price => 101, :name => 'car'})
 cart.add_item RealItem.new({:weight => 100, :price => 150, :name => 'car'})
 cart.add_item RealItem.new({:weight => 100, :price => 120, :name => 'kettle'})
 
-p cart.all_cars
-p cart.all_kettles
+method = "all_cars"
+p cart.send(method)
