7818b39b9604ff207b4daf50995efdc5a57a1d0b
ProgRus <progruson@gmail.com>
2017-01-27 13:40:07 +0700

    (+) pending tests, embedded describe

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -8,13 +8,23 @@ require_relative "../app/cart"
 
 describe Cart do
 
-  it "adds items into the cart" do
-    cart = Cart.new("ruslan")
-    item1 = Item.new("kettle", price: 200)
-    item2 = Item.new("car",    price: 200)
-    cart.add_items(item1, item2)
-    # cart.items.should include(item1, item2)
-    expect(cart.items).to include(item1, item2)
+  describe "Managing items" do
+
+    it "adds items into the cart" do
+      cart = Cart.new("ruslan")
+      item1 = Item.new("kettle", price: 200)
+      item2 = Item.new("car",    price: 200)
+      cart.add_items(item1, item2)
+      # cart.items.should include(item1, item2)
+      expect(cart.items).to include(item1, item2)
+    end
+
+    it "removes items from itself"
+
   end
+
+  it "counts items in itself"
+  it "places order using all the items that were added into the cart"
+  it "clears itself off the items after an order is placed"
 
 end
